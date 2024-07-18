41f9225619af5fc2f5a126a4a8525b23ed79e31e
Nikola Stankovic <jonnywintech@gmail.com>
2024-07-18 12:01:03 +0200

    added badge with numbers

--- app/Filament/Resources/EmployeeResource/Pages/ListEmployees.php
+++ app/Filament/Resources/EmployeeResource/Pages/ListEmployees.php
@@ -3,10 +3,11 @@
 namespace App\Filament\Resources\EmployeeResource\Pages;
 
 use Filament\Actions;
+use App\Models\Employee;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 use App\Filament\Resources\EmployeeResource;
 use Filament\Resources\Pages\ListRecords\Tab;
-use Illuminate\Database\Eloquent\Builder;
 
 class ListEmployees extends ListRecords
 {
@@ -24,11 +25,14 @@ class ListEmployees extends ListRecords
         return [
             'All' => Tab::make(),
             'This Week' => Tab::make()
-            ->modifyQueryUsing(fn(Builder $query) => $query->where('date_hired', '>', now()->subWeek() )),
+            ->modifyQueryUsing(fn(Builder $query) => $query->where('date_hired', '>', now()->subWeek()))
+            ->badge(Employee::query()->where('date_hired', '>', now()->subWeek())->count()),
             'This Month' => Tab::make()
-            ->modifyQueryUsing(fn(Builder $query) => $query->where('date_hired', '>', now()->subMonth() )),
+            ->modifyQueryUsing(fn(Builder $query) => $query->where('date_hired', '>', now()->subMonth()))
+            ->badge(Employee::query()->where('date_hired', '>', now()->subMonth())->count()),
             'This Year' => Tab::make()
-            ->modifyQueryUsing(fn(Builder $query) => $query->where('date_hired', '>', now()->subYear() )),
+            ->modifyQueryUsing(fn(Builder $query) => $query->where('date_hired', '>', now()->subYear()))
+            ->badge(Employee::query()->where('date_hired', '>', now()->subYear())->count())
         ];
 
     }
